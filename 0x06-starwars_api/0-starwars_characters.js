ac768cd33cb2bc2d1e7fc8e18037141292ef5f59
TheOrbMaverick <enangita@gmail.com>
2024-08-08 03:05:28 +0100

    execution

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,22 +1,35 @@
-#!/usr/bin/node
+#!/usr/local/bin/node
 
 const request = require('request');
+
 const movieId = process.argv[2];
 const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
 
-request(apiUrl, function (error, response, body) {
+request(apiUrl, (error, response, body) => {
   if (error) {
-    console.log(error);
+    console.error(error);
     return;
   }
+
   const characters = JSON.parse(body).characters;
-  characters.forEach(character => {
-    request(character, function (error, response, body) {
-      if (error) {
-        console.log(error);
-        return;
-      }
-      console.log(JSON.parse(body).name);
+
+  const fetchCharacterName = (url) => {
+    return new Promise((resolve, reject) => {
+      request(url, (error, response, body) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(JSON.parse(body).name);
+        }
+      });
     });
-  });
+  };
+
+  const characterPromises = characters.map((url) => fetchCharacterName(url));
+
+  Promise.all(characterPromises)
+    .then((names) => {
+      names.forEach((name) => console.log(name));
+    })
+    .catch((error) => console.error(error));
 });
