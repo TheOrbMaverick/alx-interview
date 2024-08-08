6290d96a884072ff70c81aafa9eeacbc76b4a91a
TheOrbMaverick <enangita@gmail.com>
2024-08-08 02:07:05 +0100

    star wars api in Javascript

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const request = require('request');
+const movieID = process.argv[2];
+const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
+
+request(apiUrl, function(error, response, body) {
+    if (error) {
+        console.log(error)
+        return
+    }
+    const characters = JSON.parse(body).characters;
+    characters.forEach(character => {
+        request(character, function (error, response, body) {
+            if (error) {
+                console.log(error);
+                return
+            }
+            console.log(JSON.parse(body).name)
+        })
+    });
+})
